0a5e3043250cde173a0256750f87ac65071cc501
AkotoChristine <144035257+AkotoChristine@users.noreply.github.com>
2023-12-27 20:29:07 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+//adding two numbers
+function addition(a , b){
+   return a + b;
+}
+console.log(addition(5 , 7));
+
+//substracting two numbers
+function addition(a , b){
+   return a - b;
+}
+console.log(addition(5 , 7));
+
+
+//dividing two numbers
+function addition(a , b){
+   return a / b;
+}
+console.log(addition(5 , 7));
+
+//multiplying two numbers
+function addition(a , b){
+   return a * b;
+}
+console.log(addition(5 , 7));
